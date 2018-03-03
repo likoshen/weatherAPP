bb35609373d1f816fe226f20ce04e534b510a666
caoguanjie <374348532@qq.com>
2018-03-03 12:01:52 +0800

    删除多余的页面，新增首页

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ZipagePage } from './zipage';
+import { HomePage } from './home';
 
 @NgModule({
   declarations: [
-    ZipagePage,
+    HomePage,
   ],
   imports: [
-    IonicPageModule.forChild(ZipagePage),
+    IonicPageModule.forChild(HomePage),
   ],
 })
-export class ZipagePageModule {}
+export class HomePageModule {}
